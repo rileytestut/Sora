1c142c946a7d54aa93e7ae7f903537cea770bd08
Riley Testut <riley@rileytestut.com>
2017-03-19 12:39:17 -0500

    Initial Commit

---
+++ Sora/Sora.h
@@ -0,0 +1,19 @@
+//
+//  Sora.h
+//  Sora
+//
+//  Created by Riley Testut on 3/19/17.
+//  Copyright © 2017 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sora.
+FOUNDATION_EXPORT double SoraVersionNumber;
+
+//! Project version string for Sora.
+FOUNDATION_EXPORT const unsigned char SoraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sora/PublicHeader.h>
+
+
